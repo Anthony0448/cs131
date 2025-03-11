a77fbde05cf3ab5e08ac9a4fe907297f8844f6ad
anthony <anthony.flores@sjsu.edu>
2025-03-11 21:14:31 +0000

    Fixed comments for a2 script

--- a2/script.sh
+++ a2/script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # List all files and directories (names only not exact path for display)
+# This array holds the full paths for the files in $HOME
 echo "Files and directories in $HOME:"
 items=("$HOME"/*) # Array of full paths in $HOME
 
-# ##*/ removes between and including the last '/'
-names=("${items[@]##*/}") # Extract just the names
+# Each element in ${names[]} represents an element in ${items[]}, but only the name not the full path
+names=("${items[@]##*/}") # Extract just the names ; ##*/ removes between and including the last '/'
 
-echo "Select the server directory you would like to update:"
+echo "Select the server directory you would like to work with:"
 
-# Use select to display only the names
+# Use select to provide an interactive menuu for selecting a directory instead of typing full path
 select name in "${names[@]}"; do
     if [[ -n "$name" ]]; then
-        # Map the name back to the full path
+        # Set $choice to the current $name element in the loop
         choice="$HOME/$name"
 
         # If directory
