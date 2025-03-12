64c608914953cb8b8cb5a9032e962610b6dbc1a4
anthony <anthony.flores@sjsu.edu>
2025-03-12 02:12:23 +0000

    Added test select menu for options to select from

--- a2/script.sh
+++ a2/script.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# To do
+# Schedule backups
+# Cleanup old backups after some days
+
+# Make it so the backups have the time and date in their names
+options=("Backup file or directory" "Backup & compress" "Decompress backup")
+
+select selectedOption in "${options[@]}"; do
+    if [[ -n "$selectedOption" ]]; then
+        echo "You picked: $selectedOption"
+        break
+    else
+        echo "Invalid choice. Try again."
+    fi
+done
+
 # List all files and directories (names only not exact path for display)
 # This array holds the full paths for the files in $HOME
 echo "Files and directories in $HOME:"
@@ -12,8 +28,9 @@ echo "Select the server directory you would like to work with:"
 
 # Use select to provide an interactive menuu for selecting a directory instead of typing full path
 select name in "${names[@]}"; do
+    # -n ensures the selection is nonempty
     if [[ -n "$name" ]]; then
-        # Set $choice to the current $name element in the loop
+        # Set $choice to the the full path by adding $HOME beofre $name. Needed for if statements to work
         choice="$HOME/$name"
 
         # If directory
